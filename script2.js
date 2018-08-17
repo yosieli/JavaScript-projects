e927a598c8f569df95307c06f5a3ebca55c04e96
feven tsegay <feven_teklay21@yahoo.com>
2018-08-16 21:40:25 -0500

    second java script code

---
+++ script2.js
@@ -0,0 +1,39 @@
+/* code 2 javascript */
+
+var JhoneGame1=89;
+var JhoneGame2=120;
+var JhoneGame3=103;
+var MikeGame1=116;
+var MikeGame1=94;
+var MikeGame1=123;
+var MarryGame1=97;
+var MarryGame2=134;
+var MarryGame1=105;
+var MarryAverage=(97+134+105)/3;
+var JoneAverage=(89+120+103)/3;
+var MikeAverage=(116+94+123)/3;
+if(JoneAverage > MarryAverage && JoneAverage > MikeAverage ){
+    console.log("winner is Jone with average score"   + '  ' +JoneAverage);
+}
+else if(MarryAverage > JoneAverage && MarryAverage > MikeAverage){
+    
+        console.log("winner  is Marry with average score" + '  ' + MarryAverage);
+         }
+else{
+   console.log("winner is Mike with average score" + ' '+MikeAverage);                 
+                    }
+  
+                    
+                    
+                    
+                 
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+
